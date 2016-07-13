7f7fab559ab8e77256ed6c166ba8a154ec920683
Your Name <bondarenko_igory@mail.ru>
2016-07-13 17:14:02 +0300

    nn

--- server.js
+++ server.js
@@ -1,74 +1,24 @@
-var http = require('http');
-var fs = require('fs');
-var path = require('path');
-var mime = require('mime');
-var cluster = require('cluster');
-var numCPUs = require('os').cpus().length;
-var chatServer = require('./lib/chat_server');
+var connect = require('connect'),
+    serveStatic = require('serve-static');
 
-var cache = {};
+var app = connect();
 
-if(cluster.isMaster){
-    for(var i = 0;i<numCPUs;i++){
-        cluster.fork();
-    }
-    cluster.on('exit',function(worker,code,signal) {
-        console.log('worker exited with error code:' + code );
+app.use(serveStatic("./angularjs"));
+app.listen(5000);
+/*
+var net = require('net');
+var HOST = '127.0.0.1';
+var PORT = 6969;
+net.createServer(function(sock) {
+    console.log('CONNECTED: ' + sock.remoteAddress +':'+ sock.remotePort);
+    sock.on('data', function(data) {
+        console.log('DATA ' + sock.remoteAddress + ': ' + data);
+        sock.write('You said "' + data + '"');
     });
-}else{
-    var server = http.createServer(function (req, res) {
-        var filePath = false;
-        if (req.url == '/') {
-            filePath = 'public/index.json'
-        } else {
-            filePath = 'public' + req.url;
-        }
-        var absPath = './' + filePath;
-        serveStatic(res, cache, absPath);
+    sock.on('close', function(data) {
+        console.log('CLOSED: ' + sock.remoteAddress +' '+ sock.remotePort);
     });
 
-    server.listen(3000, function () {
-        console.log('listening');
-    });
-}
-process.on('uncaughtException', function (err) {
-    console.error((new Date).toUTCString() + ' uncaughtException:', err.message)
-    console.error(err.stack)
-    process.exit(1)
-})
-
-
-    function send404(response) {
-        response.writeHead(404, {'Content-Type': 'text/plain'});
-        response.write('Error 404: resource not found');
-        response.end();
-    }
-
-    function sendFile(response, filePath, fileContents) {
-        response.writeHead(
-            200,
-            {'Content-Type': mime.lookup(path.basename(filePath))}
-        );
-        response.end();
-    }
-
-    function serveStatic(response, cache, absPath) {
-        if (cache[absPath]) {
-            sendFile(response, absPath, cache[absPath]);
-        } else {
-            fs.exists(absPath, function (exists) {
-                if (exists) {
-                    fs.readFile(absPath, function (err, data) {
-                        if (err) {
-                            send404(response);
-                        } else {
-                            cache[absPath] = data;
-                            sendFile(response, absPath, data);
-                        }
-                    })
-                } else {
-                    send404(response);
-                }
-            });
-        }
-    }
+}).listen(PORT, HOST);
+console.log('Server listening on ' + HOST +':'+ PORT);
+    */
